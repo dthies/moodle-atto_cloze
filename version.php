2b70f8bb5b04966f30a7e33d0ee0f2321ea7302d
Daniel Thies <dthies@ccal.edu>
2016-07-31 13:06:07 -0500

    Release intial Beta version

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015041604;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2016073100;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014051200;        // Requires this Moodle version.
 $plugin->component = 'atto_cloze';  // Full name of the plugin (used for diagnostics).
-$plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '2015041602';
+$plugin->maturity = MATURITY_BETA;
+$plugin->release = '1.0 Beta';

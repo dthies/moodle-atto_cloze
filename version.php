390ce4a3126820d280069078ccfed0ff3fa6ce10
Daniel Thies <dthies@ccal.edu>
2016-11-10 14:26:17 -0600

    Update version.php
    Release stable version 1.0

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016073108;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2016111000;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014051200;        // Requires this Moodle version.
 $plugin->component = 'atto_cloze';  // Full name of the plugin (used for diagnostics).
-$plugin->maturity = MATURITY_BETA;
-$plugin->release = '1.0 Beta3';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0';

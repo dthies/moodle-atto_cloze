eeed08694248cbfb12e98edafec02984ade28fcb
Daniel Thies <dethies@gmail.com>
2026-09-22 22:41:45 -0500

    Bump version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017072811;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2017072812;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2023041800;        // Requires this Moodle version.
 $plugin->component = 'atto_cloze';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
